8f9fe3c4de717853a24c866d68f569dbb96b937a
Roosterbear <lfpereag@gmail.com>
2019-12-10 09:50:30 -0600

    reporte bks otorgadas

--- reporte_becas_otorgadas.sql
+++ reporte_becas_otorgadas.sql
@@ -2,7 +2,7 @@ select
 /* HAY APELLIDOS QUE SALEN CON UN GUION, SE LOS QUITO PARA QUE NO DEN PROBLEMAS CON EXCEL */
 (replace(p.apellido_paterno,'-','')+' '+apellido_materno+' '+p.nombre) as nombre
 ,p.curp
-,bs.matricula
+,ba.matricula
 /* CONVIERTO LA FECHA A UN FORMATO MAS BONITO Y SIN LA HORA */
 ,CONVERT(VARCHAR(10), p.fecha_nacimiento, 103) as fecha_nacimiento
 /* DE LA CURP SACO EL ESTADO DONDE NACIO */
@@ -11,24 +11,24 @@ select
 ,dg.calle
 ,dg.numeroExterior as numero_exterior
 ,colonia.codigo_postal as codigo_postal
-
-/* CONVIERTO LA FECHA A UN FORMATO MAS BONITO Y SIN LA HORA */
-,CONVERT(VARCHAR(10), bs.fecha_solicitud, 103) as fecha_solicitud
-from beca_solicitud as bs
-inner join alumno a on a.matricula = bs.matricula
+,bc.porcentaje
+,CONVERT(VARCHAR(10), ba.fecha, 103) as fecha_inicio
+from beca_asignada as ba
+inner join alumno a on a.matricula = ba.matricula
 inner join persona p on p.cve_persona = a.cve_alumno
-inner join beca_convocatoria bc on bc.id = bs.id_convocatoria_beca
 inner join [admision].[dbo].[datosGenerales] dg on dg.idAdmision = p.cve_persona
+inner join beca_cat bc on ba.cve_beca_cat = bc.cve_beca_cat
 /* DE AQUI SACO EL NOMBRE DE LA COLONIA Y EL CP, ES OUTER PORQUE NO TODOS TIENEN */
 left outer join colonia colonia on colonia.cve_colonia = dg.cve_colonia
 /* EL REPORTE ES POR AÑO, DEBEMOS COMPARARLO CON EL FORMATO ORIGINAL QUE TRAE LA FECHA */
-where fecha_solicitud between '2019-01-01 00:00:00.000' and '2019-12-31 23:59:59.000'
+where ba.fecha between '2019-01-01 00:00:00.000' and '2019-12-31 23:59:59.000'
 /* LO ORDENO POR CURP PORQUE EL APELLIDO TRAE PROBLEMAS */
+and bc.cve_beca_cat < 7
 order by 2 asc
 
 
-
-
+--select * from beca_asignada
+--select * from beca_cat
 
 
 
